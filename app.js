ce74550d33cf2065e7fb4c469df04252f0347994
蔡丽纯 <12345.qq.com>
2019-12-18 22:14:40 +0800

    抽离路由router模块

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // 引入模块
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const template = require('art-template');
-//在node中提供了一个url模块专门用来处理请求的路径相关的数据
-const urlModel = require('url');
 
-// 引入用户模块
-const bindRender = require('./bindRender');
+
+// 引入bindRender用户模块
+let bindRender = require('./bindRender');
+// 引入router用户模块
+let router = require('./router');
 
 // 创建一个服务器对象
 let app = http.createServer();
@@ -22,76 +21,6 @@ app.on('request', (req, res) => {
   // 需要先调用bindRender
   bindRender(req, res);
 
-  let method = req.method;
-  let url = req.url;
-  let pathname = urlModel.parse(url, true).pathname;
-  let query = urlModel.parse(url, true).query;
-
-  if (method == 'GET' && (pathname == '/' || pathname == '/index' || pathname == '/index.html')) {
-    // 读取首页的数据
-    // fs.readFile(path.join(__dirname, './views/index.html'), (err, data) => {
-    //   if (err) return console.log(err.message);
-    //   res.end(data);
-    // })
-
-    // 模拟从数据库拿取数据
-    fs.readFile(path.join(__dirname, './heros.json'), (err, data) => {
-      if (err) return console.log(err.message);
-      let herosData = JSON.parse(data);
-      // let str = template(path.join(__dirname, './views/index.html'), {
-      //   data: herosData
-      // });
-      // res.end(str)
-
-      // 使用封装的函数
-      res.render('index', {
-        data: herosData
-      })
-    })
-  } else if (method == 'GET' && (pathname == '/edit') || pathname == '/edit.html') {
-    // 读取编辑页面的数据
-    // fs.readFile(path.join(__dirname, './views/edit.html'), (err, data) => {
-    //   if (err) return console.log(err.message);
-    //   res.end(data);
-    // })
-
-    // let str = template(path.join(__dirname, './views/edit.html'), {});
-    // res.end(str)
-
-    res.render('edit', {})
-  } else if (method == 'GET' && (pathname == '/add') || pathname == '/add.html') {
-    // 读取添加页面的数据
-    // fs.readFile(path.join(__dirname, './views/add.html'), (err, data) => {
-    //   if (err) return console.log(err.message);
-    //   res.end(data);
-    // })
-
-    // let str = template(path.join(__dirname, './views/add.html'), {});
-    // res.end(str)
-
-    res.render('add', {})
-  } else if (method == 'GET' && (pathname == '/info') || pathname == '/info.html') {
-    // 读取查看页面的数据
-    // fs.readFile(path.join(__dirname, './views/info.html'), (err, data) => {
-    //   if (err) return console.log(err.message);
-    //   res.end(data);
-    // })
-
-    // let str = template(path.join(__dirname, './views/info.html'), {});
-    // res.end(str)
-
-    res.render('info', {})
-  } else if (method == 'GET' && pathname == '/node_modules/bootstrap/dist/css/bootstrap.css') {
-    fs.readFile(path.join(__dirname, './node_modules/bootstrap/dist/css/bootstrap.css'), (err, data) => {
-      if (err) return console.log(err.message);
-      res.end(data);
-    })
-  } else if (method == 'GET' && pathname == '/node_modules/jquery/dist/jquery.js') {
-    fs.readFile(path.join(__dirname, './node_modules/jquery/dist/jquery.js'), (err, data) => {
-      if (err) return console.log(err.message);
-      res.end(data);
-    })
-  } else {
-    res.end('404');
-  }
+  // 调用路由方法
+  router(req, res);
 })
---
+++ router.js
@@ -0,0 +1,86 @@
+//路由模块只负责路由的分发，不处理具体的业务
+
+// 引入所需模块
+const fs = require('fs');
+const path = require('path');
+//在node中提供了一个url模块专门用来处理请求的路径相关的数据
+const urlModel = require('url');
+
+// 封装一个路由方法
+function router(req, res) {
+  let method = req.method;
+  let url = req.url;
+  let pathname = urlModel.parse(url, true).pathname;
+  let query = urlModel.parse(url, true).query;
+
+  if (method == 'GET' && (pathname == '/' || pathname == '/index' || pathname == '/index.html')) {
+    // 读取首页的数据
+    // fs.readFile(path.join(__dirname, './views/index.html'), (err, data) => {
+    //   if (err) return console.log(err.message);
+    //   res.end(data);
+    // })
+
+    // 模拟从数据库拿取数据
+    fs.readFile(path.join(__dirname, './heros.json'), (err, data) => {
+      if (err) return console.log(err.message);
+      let herosData = JSON.parse(data);
+      // let str = template(path.join(__dirname, './views/index.html'), {
+      //   data: herosData
+      // });
+      // res.end(str)
+
+      // 使用封装的函数
+      res.render('index', {
+        data: herosData
+      })
+    })
+  } else if (method == 'GET' && (pathname == '/edit') || pathname == '/edit.html') {
+    // 读取编辑页面的数据
+    // fs.readFile(path.join(__dirname, './views/edit.html'), (err, data) => {
+    //   if (err) return console.log(err.message);
+    //   res.end(data);
+    // })
+
+    // let str = template(path.join(__dirname, './views/edit.html'), {});
+    // res.end(str)
+
+    res.render('edit', {})
+  } else if (method == 'GET' && (pathname == '/add') || pathname == '/add.html') {
+    // 读取添加页面的数据
+    // fs.readFile(path.join(__dirname, './views/add.html'), (err, data) => {
+    //   if (err) return console.log(err.message);
+    //   res.end(data);
+    // })
+
+    // let str = template(path.join(__dirname, './views/add.html'), {});
+    // res.end(str)
+
+    res.render('add', {})
+  } else if (method == 'GET' && (pathname == '/info') || pathname == '/info.html') {
+    // 读取查看页面的数据
+    // fs.readFile(path.join(__dirname, './views/info.html'), (err, data) => {
+    //   if (err) return console.log(err.message);
+    //   res.end(data);
+    // })
+
+    // let str = template(path.join(__dirname, './views/info.html'), {});
+    // res.end(str)
+
+    res.render('info', {})
+  } else if (method == 'GET' && pathname == '/node_modules/bootstrap/dist/css/bootstrap.css') {
+    fs.readFile(path.join(__dirname, './node_modules/bootstrap/dist/css/bootstrap.css'), (err, data) => {
+      if (err) return console.log(err.message);
+      res.end(data);
+    })
+  } else if (method == 'GET' && pathname == '/node_modules/jquery/dist/jquery.js') {
+    fs.readFile(path.join(__dirname, './node_modules/jquery/dist/jquery.js'), (err, data) => {
+      if (err) return console.log(err.message);
+      res.end(data);
+    })
+  } else {
+    res.end('404');
+  }
+}
+
+//暴露路由方法
+module.exports = router;
