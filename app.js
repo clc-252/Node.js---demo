e5dc0dc1c4e06a6e0829ddd77f8e3b348dcc4237
蔡丽纯 <12345.qq.com>
2019-12-18 22:00:58 +0800

    创建bindRender用户模块

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const template = require('art-template');
 //在node中提供了一个url模块专门用来处理请求的路径相关的数据
 const urlModel = require('url');
 
+// 引入用户模块
+const bindRender = require('./bindRender');
+
 // 创建一个服务器对象
 let app = http.createServer();
 
@@ -16,6 +19,9 @@ app.listen(3000, () => {
 
 // 注册一个监听用户请求的事件
 app.on('request', (req, res) => {
+  // 需要先调用bindRender
+  bindRender(req, res);
+
   let method = req.method;
   let url = req.url;
   let pathname = urlModel.parse(url, true).pathname;
@@ -27,14 +33,20 @@ app.on('request', (req, res) => {
     //   if (err) return console.log(err.message);
     //   res.end(data);
     // })
+
     // 模拟从数据库拿取数据
     fs.readFile(path.join(__dirname, './heros.json'), (err, data) => {
       if (err) return console.log(err.message);
       let herosData = JSON.parse(data);
-      let str = template(path.join(__dirname, './views/index.html'), {
+      // let str = template(path.join(__dirname, './views/index.html'), {
+      //   data: herosData
+      // });
+      // res.end(str)
+
+      // 使用封装的函数
+      res.render('index', {
         data: herosData
-      });
-      res.end(str)
+      })
     })
   } else if (method == 'GET' && (pathname == '/edit') || pathname == '/edit.html') {
     // 读取编辑页面的数据
@@ -42,24 +54,33 @@ app.on('request', (req, res) => {
     //   if (err) return console.log(err.message);
     //   res.end(data);
     // })
-    let str = template(path.join(__dirname, './views/edit.html'), {});
-    res.end(str)
+
+    // let str = template(path.join(__dirname, './views/edit.html'), {});
+    // res.end(str)
+
+    res.render('edit', {})
   } else if (method == 'GET' && (pathname == '/add') || pathname == '/add.html') {
     // 读取添加页面的数据
     // fs.readFile(path.join(__dirname, './views/add.html'), (err, data) => {
     //   if (err) return console.log(err.message);
     //   res.end(data);
     // })
-    let str = template(path.join(__dirname, './views/add.html'), {});
-    res.end(str)
+
+    // let str = template(path.join(__dirname, './views/add.html'), {});
+    // res.end(str)
+
+    res.render('add', {})
   } else if (method == 'GET' && (pathname == '/info') || pathname == '/info.html') {
     // 读取查看页面的数据
     // fs.readFile(path.join(__dirname, './views/info.html'), (err, data) => {
     //   if (err) return console.log(err.message);
     //   res.end(data);
     // })
-    let str = template(path.join(__dirname, './views/info.html'), {});
-    res.end(str)
+
+    // let str = template(path.join(__dirname, './views/info.html'), {});
+    // res.end(str)
+
+    res.render('info', {})
   } else if (method == 'GET' && pathname == '/node_modules/bootstrap/dist/css/bootstrap.css') {
     fs.readFile(path.join(__dirname, './node_modules/bootstrap/dist/css/bootstrap.css'), (err, data) => {
       if (err) return console.log(err.message);
